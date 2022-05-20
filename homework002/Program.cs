31c92e0aa0ddf2391ca7bbec365602ef69bfce93
sirgak <life_4_ever@mail.ru>
2022-05-20 18:43:41 +0300

    test
    vse eshe test

---
+++ homework002/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace homework002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string age;
+            string zodiacSigh;
+            string job;
+            Console.Write("Как вас зовут?");
+            name = Console.ReadLine();
+            Console.Write("Сколько вам лет?");
+            age = Console.ReadLine();
+            Console.Write("Кто вы по гороскопу?");
+            zodiacSigh = Console.ReadLine();
+            Console.Write("Где вы работаете?");
+            job = Console.ReadLine();
+            Console.WriteLine("Вас зовут - " + name + "вам" + age + "лет" + "по гороскопу вы " + zodiacSigh + "вы работаете" + job);
+        }
+    }
+}
